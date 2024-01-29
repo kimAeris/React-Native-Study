f994eecff159b7fea687eaed095df09468043884
KimAeris <kar2125@naver.com>
2024-01-30 00:19:26 +0900

    Todo UI 생성

---
+++ screens/MainScreen.js
@@ -0,0 +1,56 @@
+import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import InputForm from '../components/InputForm'
+import TodoItem from '../components/TodoItem'
+
+const MainScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={'default'} />
+      <Text style={styles.pageTitle}>ToDo App</Text> 
+      <View style={styles.listView}>
+        <Text style={styles.listTitle}>할 일</Text>
+        <TodoItem />
+      </View>
+      <View style={styles.separator} />
+      <View style={styles.listView}>
+        <Text style={styles.listTitle}>완료된 일</Text>
+      </View>
+      <InputForm />
+    </SafeAreaView>
+  )
+}
+
+export default MainScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // 아이폰,안드로이드 분기 처리 -> SafeAreaView를 사용하여 아이폰은 이미 top 효과를 가졌으므로 안드로이드만 따로 처리해준다.
+    paddingTop: Platform.OS === 'android' ? 20 : 0,
+    backgroundColor: '#f7f8fa'
+  },
+  pageTitle: {
+      marginBottom: 35,
+      paddingHorizontal: 15,
+      fontSize: 54,
+      fontWeight: '600'
+  },
+  separator: {
+      marginHorizontal: 10,
+      marginTop: 25,
+      marginBottom: 10,
+      borderBottomWidth: 1,
+      borderBottomColor: 'rgba(0,0,0,0.2)'
+  },
+  listView: {
+      flex: 1,
+  },
+  listTitle: {
+      marginBottom: 25,
+      paddingHorizontal: 15,
+      fontSize: 41,
+      fontWeight: '500'
+  },
+})
